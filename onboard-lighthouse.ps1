82c394f0bf2012410e34abcb89dea59194c60af1
Siddhant Bisht <Siddhantbisht9@gmail.com>
2025-07-16 01:37:43 +0530

    Add Azure Lighthouse Onbaording Script

---
+++ onboard-lighthouse.ps1
@@ -0,0 +1,31 @@
+# Script to onboard customer to Azure Lighthouse
+$managingTenantId = "Org root level tenant-ID"
+$delegatedTenantId = "customer to onboard tenant-id"
+$delegatedSubscriptionId = "customer subscription-id"
+$offerName = "MyLighthouseOffer"
+$offerDescription = "Lighthouse onboarding for support"
+$authorization = @(
+    @{
+        principalId = "give the object id of the app registration, group or user"
+        roleDefinitionId = (Get-AzRoleDefinition "Contributor").Id
+        principalIdDisplayName = "MSP Contributor Access"
+    }
+)
+
+# Create Managed Services Definition
+$managedServiceDefinition = @{
+    Name = $offerName
+    Description = $offerDescription
+    Authorizations = $authorization
+    ManagedByTenantId = $managingTenantId
+}
+
+# Create the Lighthouse delegation
+New-AzManagedServicesDefinition `
+    -Name $offerName `
+    -ManagedByTenantId $managedServiceDefinition.ManagedByTenantId `
+    -Authorizations $managedServiceDefinition.Authorizations `
+    -Description $managedServiceDefinition.Description `
+    -Scope "/subscriptions/$delegatedSubscriptionId"
+
+Write-Host "Lighthouse delegation successfully created!"
